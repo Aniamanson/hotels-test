fa19340567eebfe3508f4306a6bf6bb0a2fe6f6d
Anna Vershkova <a.vershkova@monodigital.ru>
2021-12-25 20:43:29 +0300

    config linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -16,5 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     indent: ['warn', 2],
     quotes: ['warn', 'single', { avoidEscape: true }],
+    'arrow-parens': ['warn', 'as-needed'],
+    '@typescript-eslint/ban-types': ['warn'],
   },
 };
